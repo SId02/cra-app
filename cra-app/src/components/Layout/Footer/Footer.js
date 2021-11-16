aa09bf7bf9cd4a02e1b4d5c0697ef95a594fd854
Siddhesh M <31335947+SId02@users.noreply.github.com>
2021-11-16 16:17:57 +0530

    update add pagination

---
+++ cra-app/src/components/Layout/Footer/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+	return (
+		<>
+			{/* <footer className="py-3 mt-auto text-center footer bg-dark"> */}
+			<footer className="py-3 text-center footer bg-dark">
+				<div className="container">
+					<p className="text-white">CRA APP</p>
+				</div>
+			</footer>	
+		</>
+	);
+};
+
+export default Footer;
---
+++ cra-app/src/components/Pages/APICall.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from 'react'
+// import { NavLink } from "react-router-dom";
+import Loading from '../Layout/Github-User-Card/Loading.js';
+import GithubUserCards from "../Layout/Github-User-Card/Github-User-Card";
+
+const APICall = () => {
+  
+  const [users, setUsers] = useState([]);
+ const [loading, setLoading] = useState(true);
+
+  const getUsers = async () => {
+      try {
+          const response = await fetch('https://api.github.com/users');
+           setLoading(false);
+          setUsers(await response.json());
+      } catch (error) {
+          setLoading(false);
+          console.log("my error is "+ error);
+      }
+  }
+
+  useEffect(() => {
+      getUsers();
+  }, []);
+
+  if (loading) {
+      return <Loading />
+  }
+  return (
+    <>
+          <GithubUserCards users={users}/>
+    </>
+  );
+};
+
+export default APICall;
